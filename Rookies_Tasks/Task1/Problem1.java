bcb44bdd15ca03df5fd8a6dc3e0618df0b2ff633
anton-onsy <antonking249@gmail.com>
2024-08-02 20:50:00 +0300

    first problem Done

--- Rookies_Tasks/Task1/Problem1.java
+++ Rookies_Tasks/Task1/Problem1.java
@@ -1,5 +1,24 @@
 public class Problem1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr={10,15,20,7,80,8,40,0,60,4};
+        int []temp=new int[arr.length];
+        for(int i=0;i< arr.length;i++) {
+            if (i == arr.length - 1) {
+                temp[i] = -1;
+                break;
+            } else {
+                int MaxNum = arr[i+1];
+                for (int j = i + 2; j < arr.length; j++) {
+                    if (arr[j] > MaxNum) {
+                        MaxNum = arr[j];
+                    }
+                }
+                temp[i] = MaxNum;
+
+            }
+        }
+        for(int i=0;i<temp.length;i++){
+            System.out.println(temp[i]);
+        }
     }
 }
